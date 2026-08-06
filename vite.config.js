70ffbd3ebf4b826b350713dee14d35fd04352388
floriansalvi <floriansalvipro@gmail.com>
2026-08-06 14:04:12 +0200

    docs: add docs directory

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    // 
+    plugins: [vue()],
+    // Configuration des tests Vitest.
     test: {
         globals: true,
         environment: "jsdom",
